2d6893dd5ec47075b2ceac0d86bfdd7cde999e2e
Darsh <darshrawat03@gmail.com>
2023-01-20 14:17:23 +0530

    correct b_search

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -3,34 +3,28 @@
 
 using namespace std;
 
-int binarySearch(int arr[], int find, int start, int end){
-    if(end >= 1) {
-        int mid = start + (end - 1) / 2;
-        if(arr[mid] == find) return mid;
-        if(arr[mid] > find){
-            
-            //cout<<"Searching "<<i<<" \n";
-            binarySearch(arr, find, start, mid - 1);
-        }
-        if(arr[mid] < find){
-            //cout<<"Searching "<<i<<" \n";
-            binarySearch(arr, find, mid + 1, end);
-        }
-        return -1;
+int binarySearch(int arr[], int find, int start, int end)
+{
+    if (end >= start) {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == find) return mid;
+        if (arr[mid] > find) return binarySearch(arr, find, start, mid - 1);
+        return binarySearch(arr, find, mid + 1, end);
     }
+    return -1;
 }
 
 int main(){
     int size, n;
     cout<<"Enter the size of the array: ";
     cin>>size;
-    cout<<"Enter the element to be found: ";
-    cin>>n;
-    int arr[n];
+    int arr[size];
     cout<<"Enter the elements: ";
     for(int i = 0; i < size; i++){
         cin>>arr[i];
     }
+    cout<<"Enter the element to be found: ";
+    cin>>n;
     cout<<binarySearch(arr, n, 0, size - 1);
     return 0;
 }
