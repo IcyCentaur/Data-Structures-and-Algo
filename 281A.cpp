8baeb8d623caf16fc7e608096aaef0955522a1a1
Darsh <darshrawat03@gmail.com>
2023-04-04 18:59:56 +0530

    commit

---
+++ 281A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(){
+    string s;
+    cin>>s;
+    s[0] = toupper(s[0]);
+    cout<<s<<endl;
+    return 0;
+}
+
+int main(){
+    solve();
+    return 0;
+} 
