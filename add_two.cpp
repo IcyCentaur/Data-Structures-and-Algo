589f12ef441baaaec2b5836dbec27a71c15f0018
Darsh <darshrawat03@gmail.com>
2023-07-24 20:49:06 +0530

    Create add_two.cpp

---
+++ add_two.cpp
@@ -0,0 +1,35 @@
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode* dummy = new ListNode(0);
+    ListNode* current = dummy;
+    int carry = 0;
+
+    while (l1 || l2 || carry) {
+        int sum = carry;
+        if (l1) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if (l2) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        carry = sum / 10;
+        sum = sum % 10;
+
+        current->next = new ListNode(sum);
+        current = current->next;
+    }
+
+    return dummy->next;
+}
+
+ListNode* createLinkedList(int arr[], int size) {
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* current = head;
+    for (int i = 1; i < size; ++i) {
+        current->next = new ListNode(arr[i]);
+        current = current->next;
+    }
+    return head;
+}
